85a6e03a715fdbd8fc6896cd2fcb1e98a5bc5cad
Brace Bade <38501867+badebh@users.noreply.github.com>
2019-01-19 16:11:36 -0500

    Success! I was able to retrieve the requested activity from Strava.

--- strava_api_learning.py
+++ strava_api_learning.py
@@ -1,19 +1,18 @@
 from oauth2_learning import token
-
-print(token)
-
-# attempt to integrate w/ strava
 import time
 import swagger_client
 from swagger_client.rest import ApiException
 from pprint import pprint
 
 # Configure OAuth2 access token for authorization: strava_oauth
-swagger_client.configuration.access_token = token['access_token']
+configuration = swagger_client.Configuration()
+configuration.access_token = token['access_token']
+
 
 # create an instance of the API class
-api_instance = swagger_client.ActivitiesApi()
-id = 2069849358 # Long | The identifier of the activity.
+api_instance = swagger_client.ActivitiesApi(swagger_client.ApiClient(configuration))
+# activity = BYT before the rain 1/18/2019
+id = 2085461936 # Long | The identifier of the activity.
 
 try: 
     # Get Activity
